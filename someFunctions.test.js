20b5ba13f681f7e9ff0d845efdb63e7a8b772fc4
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2023-05-31 15:26:46 -0400

    Divide tests into blocks

--- someFunctions.test.js
+++ someFunctions.test.js
@@ -1,34 +1,38 @@
 import someFunctions from "./someFunctions";
 
 // capitalize tests
-// Basic functionality
-test("capitalize should return a string with first character capitalized", () => {
-  expect(someFunctions().capitalize("string")).toBe("String");
-});
+describe("capitalize", () => {
+  // Basic functionality
+  test("capitalize should return a string with first character capitalized", () => {
+    expect(someFunctions().capitalize("string")).toBe("String");
+  });
 
-// Edge cases and errors
-test("passing string with spaces should work", () => {
-  expect(someFunctions().capitalize(" string")).toBe("String");
-});
+  // Edge cases and errors
+  test("passing string with spaces should work", () => {
+    expect(someFunctions().capitalize(" string")).toBe("String");
+  });
 
-test("passing empty string should return empty string", () => {
-  expect(someFunctions().capitalize("")).toBe("");
-});
+  test("passing empty string should return empty string", () => {
+    expect(someFunctions().capitalize("")).toBe("");
+  });
 
-test("passing undefined returns undefined", () => {
-  expect(someFunctions().capitalize(undefined)).toBeUndefined();
-});
+  test("passing undefined returns undefined", () => {
+    expect(someFunctions().capitalize(undefined)).toBeUndefined();
+  });
 
-test("passing null returns undefined", () => {
-  expect(someFunctions().capitalize(null)).toBeUndefined();
-});
+  test("passing null returns undefined", () => {
+    expect(someFunctions().capitalize(null)).toBeUndefined();
+  });
 
-test("passing value other than string returns undefined", () => {
-  expect(someFunctions().capitalize(57)).toBeUndefined();
+  test("passing value other than string returns undefined", () => {
+    expect(someFunctions().capitalize(57)).toBeUndefined();
+  });
 });
 
 // reverseString tests
-// Functionality
-test("passed string should be returned in reversed order", () => {
-  expect(someFunctions().reverseString("abcd")).toBe("dcba");
+describe("reverseString", () => {
+  // Functionality
+  test("passed string should be returned in reversed order", () => {
+    expect(someFunctions().reverseString("abcd")).toBe("dcba");
+  });
 });
