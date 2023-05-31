fabf9adb0af3120051fb16647b2807fd12f660ec
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2023-05-31 15:41:20 -0400

    Add more tests for reverseString

--- someFunctions.test.js
+++ someFunctions.test.js
@@ -2,12 +2,12 @@ import someFunctions from "./someFunctions";
 
 // capitalize tests
 describe("caapitalize", () => {
-  // Basic functionality
+  // Functionality
   test("capitalize should return a string with first character capitalized", () => {
     expect(someFunctions().capitalize("string")).toBe("String");
   });
 
-  // Edge cases and errors
+  // Edge cases
   test("passing string with spaces should work", () => {
     expect(someFunctions().capitalize(" string")).toBe("String");
   });
@@ -16,6 +16,7 @@ describe("caapitalize", () => {
     expect(someFunctions().capitalize("")).toBe("");
   });
 
+  // Errors
   test("passing undefined returns undefined", () => {
     expect(someFunctions().capitalize(undefined)).toBeUndefined();
   });
@@ -34,5 +35,31 @@ describe("reverseString", () => {
   // Functionality
   test("passed string should be returned in reversed order", () => {
     expect(someFunctions().reverseString("abcd")).toBe("dcba");
+  });
+
+  // Edge cases
+  test("internal spaces shouldn't impact outcome", () => {
+    expect(someFunctions().reverseString("ab c de f")).toBe("f ed c ba");
+  });
+
+  test("leading/trailing spaces sholdn't impact outcome", () => {
+    expect(someFunctions().reverseString(" abcdef ")).toBe(" fedcba ");
+  });
+
+  test("empty string returns empty string", () => {
+    expect(someFunctions().reverseString("")).toBe("");
+  });
+
+  // Errors
+  test("passing undefined returns undefined", () => {
+    expect(someFunctions().reverseString(undefined)).toBeUndefined();
+  });
+
+  test("passing null returns undefined", () => {
+    expect(someFunctions().reverseString(null)).toBeUndefined();
+  });
+
+  test("passing value other than string returns undefined", () => {
+    expect(someFunctions().reverseString(57)).toBeUndefined();
   });
 });
